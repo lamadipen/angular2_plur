7fbef868e61461dd1e4c9012f79c57ffbd704a68
Dipen Lama <lamadipen@yahoo.com>
2017-08-04 16:36:06 -0500

    A103 addingboth in component template and external template (template,templateUrl)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+import { ProductListComponent } from './products/product-list.component'
+
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <div>
+            <h1>{{ pageTitle }}</h1>
+        </div>
+        <div>
+            This is the first page
+        </div> 
+         <pm-products></pm-products>   
     `
+    
+    
 })
-export class AppComponent { }
+export class AppComponent { 
+    pageTitle: string = "Acme Product Management";
+}
